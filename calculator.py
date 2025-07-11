496bc0699ce87ee0ebe3f27d43603377dc43ba72
mayarai108 <mr6116464@gmail.com>
2025-07-11 16:23:27 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a= 5
+b= 2
+
+print("the additon of a+b = ",a+b)
+print("the subtraction  of a-b = ",a-b)
+print("the multiplication of a*b = ",a*b)
+print("the divison of a/b = ",a/b)
+print("the floor divison of a//b = ",a//b)
+print("the moduls of a%b = ",a%b)
